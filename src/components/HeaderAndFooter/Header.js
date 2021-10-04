ee654d2a5549eed138aeafed60f1621629bd390e
sunjid-git <sunjid.info@gmail.com>
2021-10-04 13:08:48 +0600

    nav link

--- src/components/HeaderAndFooter/Header.js
+++ src/components/HeaderAndFooter/Header.js
@@ -1,16 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Header.css";
 
 const Header = () => {
   return (
     <header>
-      <div className="header-left">
-      <h1>𝓢𝓽𝔂𝓵𝓮 𝓗𝓾𝓽 </h1>
-      </div>
-      <div className="header-right">
-        <nav>
-          <li></li>
-        </nav>
+      <div className="nav-elements">
+        <h1>𝓢𝓽𝔂𝓵𝓮 𝓗𝓾𝓽 </h1>
+
+        <div className="nav-links">
+         
+            <Link to="/">Home</Link>
+            <Link to="/">About</Link>
+            <Link to="/">Contact</Link>
+            <Link to="/">Login</Link>
+        
+        </div>
+
       </div>
     </header>
   );
